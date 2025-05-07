b41f639f5ca71489895b3864ffe0f0360ea51245
ReRaXX <118262117+ReRaXX@users.noreply.github.com>
2025-05-07 21:53:04 +0300

    feat: added 100 variables and completed task 2

---
+++ develop/index.js
@@ -0,0 +1,164 @@
+
+//первый
+const num1 = 1;
+const str1 = "hello";
+const bool1 = true;
+const null1= null;
+const undef1 = undefined;
+const sym1 = Symbol("a");
+const big1 = 10n;
+const obj1 = { a: 1 };
+const arr1 = [1, 2, 3];
+const func1 = function() {return 1; };
+
+//Второй
+
+const sum2 = 2;
+const str2 = "two";
+const bool2 = false;
+const null2 = null;
+const undef2 = undefined;
+const Sym2 = Symbol("b");
+const big2 = 20n;
+const obj2 = {b: 2 };
+const arr2 = [3, 5, 6];
+const func2 = function() {return 2; };
+
+//3
+
+const sum3 = 3;
+const srt3 = "three";
+const bool3 = true;
+const null3 = null;
+const undef3 = undefined;
+const sym3 = Symbol("c");
+const big3 = 30n;
+const obj3 = {c: 3 };
+const arr3 = [7, 8, 9];
+const func3 = function () {return 3; };
+
+//4
+
+const sum4 = 4;
+const str4 = "four";
+const bool4= false;
+const null4 = null;
+const undef4 = undefined;
+const sym4 = Symbol("1");
+const big4 = 40n;
+const obj4 = {d: 5};
+const arr4 = [10, 11, 12];
+const func4 = function() {return 0; };
+
+//4
+
+const sum5 = 5;
+const str5 = "five";
+const bool5 = true;
+const null5 = null;
+const undef5 = undefined;
+const big5 = 50n;
+const sym5 = Symbol("e");
+const obj5 = {objection: 12};
+const arr5 = [20, 30, 40];
+const func5 = function() {return 0;};
+
+
+const sum6 = 6;
+const str6 = "six"
+const bool6 = true;
+const null6 = null;
+const undef6 = undefined;
+const sym6 = Symbol("fff");
+const big6 = 60n;
+const obj6 = {band: "su-7"};
+const arr6 = [12, 13, 11];
+const func6 = function(){return 0;} ;
+
+//7
+
+const sum7 = 70;
+const str7 = "seven";
+const bool7 = true;
+const null7 = null;
+const undef7 = undefined;
+const sym7 = Symbol("aac")
+const big7 = 70n;
+const obj7 = {bounce: 12};
+const arr7 = [1, 11, 111]
+const func7 = function(){console.log(arr7)}
+
+//8
+
+const sum8 = 8;
+const str8 = "eight";
+const bool8 = true;
+const null8 = null;
+const undef8 = undefined;
+const sym8 = Symbol("aacc")
+const big8 = 80n;
+const obj8 = {meta: "Yve"};
+const arr8 = [2, 22, 222];
+const func8 = function() {return 'zero';};
+
+//9
+
+const sym9 = 9;
+const str9 = "nine";
+const bool9 = true;
+const undef9 = undefined;
+const big9 = 90n;
+const sum9 = Symbol("acfc")
+const obj9 = {hex: "ruin"};
+const arr9 = [1, 31, 1231];
+const func9 = function() {return "zero"; };
+
+//10
+
+const sum10 = 10;
+const str10 = "ten";
+const bool10 = false;
+const null10 = null;
+const undef10 = undefined;
+const big10 = 100n;
+const sym10 = Symbol("10");
+const obj10 = {nullik: "bullik"}
+const arr10 = [10, "array", "baton"]
+const func10 = function() {return "zero"; };
+
+
+
+const age = 25;
+const userName = "Mikhail Krivtsov";
+const isStudent = true;
+const mySymbol = Symbol("ABC");
+const BigNumber = 1000000n;
+const person = {age: age, userName: userName};
+const colors = [ "#FFFFFF" , "#FF0000", "#FFFF00"];
+const greet = function() {return str1; };
+const undefinedVar = undefined;
+const emptyValue = null;
+const PI = 3.1415;
+var oldShool = "JS";
+const hasAccess = false;
+const numbers = [1, 2, 3, 4, 5];
+const car = {brand: "Scoda", model: "Rapid", year: 2024 };
+const country = "Russia";
+const SayHi = function() {Kavabanga}; console.log(SayHi)
+const taskCount = 30;
+const MAX_USERS = 100;
+const fruits = ["Mango", "pineapple", "dragonfruit"];
+const temperature = 22.5;
+const isOnline = true;
+const settings = "";
+const messages = ["","",""];
+const sum = function() {1+1;};
+console.log(sum);
+const LastLogin = null;
+const UserRole = "Admin";
+const height = 400;
+const languages = ["JavaScript", "Golang", "Java"];
+const book = {title: "Harry Potter", author: "Joanne Rowling", year:"1997"};
+
+
+
