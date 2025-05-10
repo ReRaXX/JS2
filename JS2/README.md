bf8a035fa17c0db0ec010b66058e25dbd3a88c76
ReRaX <118262117+ReRaXX@users.noreply.github.com>
2025-05-10 19:47:32 +0300

    test

---
+++ JS2/README.md
@@ -0,0 +1,2 @@
+# JS2
+javascript homework 2
---
+++ JS2/develop/index.js
@@ -0,0 +1,650 @@
+//day 1
+
+//1
+const num1 = 1;
+const str1 = "hello";
+const bool1 = true;
+const null1= null;
+const undef1 = undefined;
+const sym1 = Symbol("a");
+const big1 = 10n;
+const obj1 = { a: 1 };
+const arr1 = [1, 2, 3];
+const func1 = function() {return 1; };
+
+//2
+
+const sum2 = 2;
+const str2 = "two";
+const bool2 = false;
+const null2 = null;
+const undef2 = undefined;
+const Sym2 = Symbol("b");
+const big2 = 20n;
+const obj2 = {b: 2 };
+const arr2 = [3, 5, 6];
+const func2 = function() {return 2; };
+
+//3
+
+const sum3 = 3;
+const srt3 = "three";
+const bool3 = true;
+const null3 = null;
+const undef3 = undefined;
+const sym3 = Symbol("c");
+const big3 = 30n;
+const obj3 = {c: 3 };
+const arr3 = [7, 8, 9];
+const func3 = function () {return 3; };
+
+//4
+
+const sum4 = 4;
+const str4 = "four";
+const bool4= false;
+const null4 = null;
+const undef4 = undefined;
+const sym4 = Symbol("1");
+const big4 = 40n;
+const obj4 = {d: 5};
+const arr4 = [10, 11, 12];
+const func4 = function() {return 0; };
+
+//4
+
+const sum5 = 5;
+const str5 = "five";
+const bool5 = true;
+const null5 = null;
+const undef5 = undefined;
+const big5 = 50n;
+const sym5 = Symbol("e");
+const obj5 = {objection: 12};
+const arr5 = [20, 30, 40];
+const func5 = function() {return 0;};
+
+
+const sum6 = 6;
+const str6 = "six"
+const bool6 = true;
+const null6 = null;
+const undef6 = undefined;
+const sym6 = Symbol("fff");
+const big6 = 60n;
+const obj6 = {band: "su-7"};
+const arr6 = [12, 13, 11];
+const func6 = function(){return 0;} ;
+
+//7
+
+const sum7 = 70;
+const str7 = "seven";
+const bool7 = true;
+const null7 = null;
+const undef7 = undefined;
+const sym7 = Symbol("aac")
+const big7 = 70n;
+const obj7 = {bounce: 12};
+const arr7 = [1, 11, 111]
+const func7 = function(){console.log(arr7)}
+
+//8
+
+const sum8 = 8;
+const str8 = "eight";
+const bool8 = true;
+const null8 = null;
+const undef8 = undefined;
+const sym8 = Symbol("aacc")
+const big8 = 80n;
+const obj8 = {meta: "Yve"};
+const arr8 = [2, 22, 222];
+const func8 = function() {return 'zero';};
+
+//9
+
+const sym9 = 9;
+const str9 = "nine";
+const bool9 = true;
+const undef9 = undefined;
+const big9 = 90n;
+const sum9 = Symbol("acfc")
+const obj9 = {hex: "ruin"};
+const arr9 = [1, 31, 1231];
+const func9 = function() {return "zero"; };
+
+//10
+
+const sum10 = 10;
+const str10 = "ten";
+const bool10 = false;
+const null10 = null;
+const undef10 = undefined;
+const big10 = 100n;
+const sym10 = Symbol("10");
+const obj10 = {nullik: "bullik"}
+const arr10 = [10, "array", "baton"]
+const func10 = function() {return "zero"; };
+
+
+
+const age = 25;
+const userName = "Mikhail Krivtsov";
+const isStudent = true;
+const mySymbol = Symbol("ABC");
+const BigNumber = 1000000n;
+const person = {age: age, userName: userName};
+const colors = [ '#FFFFFF' , '#FF0000', '#FFFF00'];
+const greet = function() {return str1; };
+const undefinedVar = undefined;
+const emptyValue = null;
+const PI = 3.1415;
+var oldShool = "JS";
+const hasAccess2 = false;
+const numbers = [1, 2, 3, 4, 5];
+const car = {brand: "Scoda", model: "Rapid", year: 2024 };
+const country = "Russia";
+const SayHi = function() {Kavabanga}; console.log(SayHi)
+const taskCount = 30;
+const MAX_USERS = 100;
+const fruits = ["Mango", "pineapple", "dragonfruit"];
+const temperature = 22.5;
+const isOnline = true;
+const settings = "";
+const messages = ["","",""];
+const sum = 14 + 15; console.log(sum);
+const LastLogin = null;
+const UserRole = "Admin";
+const height = 400;
+const languages = ["JavaScript", "Golang", "Java"];
+const book = {title: "Harry Potter", author: "Joanne Rowling", year:"1997"};
+
+
+
+//Day 2
+
+//11
+
+const sum11 = 11;
+const str11 = "Eleven";
+const bool11 = true;
+const null11 = null;
+const undef11 = undefined;
+const big11 = 110n;
+const sym11 = Symbol("Border")
+const obj11 = {object: "Eleven"};
+const arr11 = [111, 112, 113];
+const func11 = function() {return 0;};
+
+//12
+
+const sum12 = 12;
+const str12 = "twelve";
+const bool12 = false;
+const big12 = 120n;
+const null12 = null;
+const undef12 = undefined;
+const sym12 = Symbol("twelve");
+const obj12 = {reborn: "zero"};
+const arr12 = [122, 1212, 121];
+const func12 = function() {return zero; };
+
+//13
+
+const sum13 = 13;
+const str13 = "string";
+const bool13 = false;
+const big13 = 130n;
+const sym13 = Symbol("132")
+const null13 = null;
+const undef13 = undefined;
+const obj13 = {id: "n"};
+const arr13 = [133, 1333, 13333];
+const func13 = function(){return zero; };
+
+//14
+
+const sum14 = 14;
+const str14 = "fourteen";
+const bool14 = true;
+const null14 = null;
+const undef14 = undefined;
+const big14 = 140n;
+const sym14 = Symbol("afsfs");
+const obj14 = {brute: "force"};
+const arr14 = [14, 144, 1444];
+const func = function() {return zero; };
+
+//15
+
+const sum15 = 15;
+const str15 = "fifteen";
+const bool15 = true;
+const null15 = null;
+const undef15 = undefined;
+const big15 = 150n;
+const sym15 = Symbol("fifteen");
+const obj15 = {fif: "teen"};
+const arr15 = [15, 155, 1555];
+const func15 = function() {return zero; };
+
+//16
+
+const sum16 = 16;
+const str16 = "sixteen";
+const bool16 = true;
+const null16 = null;
+const undef16 = undefined;
+const big16 = 160n;
+const sym16 = Symbol("sixteen");
+const obj16 = {six: "teen"};
+const arr16 = [16, 166, 1666];
+const func16 = function() {return zero; };
+
+//17
+
+const sum17 = 17;
+const str17 = "seventeen";
+const bool17 = true;
+const null17 = null;
+const undef17 = undefined;
+const big17 = 170n;
+const sym17 = Symbol("sweden");
+const obj17 = {seven: "teen"};
+const arr17 = [17, 177, 1777];
+const func17 = function() {return zero; };
+
+//18
+
+const sum18 = 18;
+const str18 = "eighteen";
+const bool18 = false;
+const null18 = null;
+const undef18 = undefined;
+const big18 = 180n;
+const sym18 = Symbol("eighteen");
+const obj18 = {bla: "blabla"};
+const arr18 = [18, 188, 1888];
+const func18 = function() {return zero; };
+
+//19
+
+const sum19 = 19;
+const str19 = "nineteen";
+const bool19 = true;
+const null19 = null;
+const undef19 = undefined;
+const big19 = 190n;
+const sym19 = Symbol("nineteen");
+const obj19 = {nine: "old"};
+const arr19 = [19, 199, 1999];
+const func19 = function() {return zero; };
+
+//20
+
+const sum20 = 20;
+const str20 = "twenty";
+const bool20 = false;
+const null20 = null;
+const indef20 = undefined;
+const big20 = 200n;
+const sym20 = Symbol("twenty");
+const obj20 = {twen: "ty"};
+const arr = [20, "test", "Boneca"];
+const func20 = function() {return zero; };
+
+
+
+const age2 = 25;
+const userName2 = "KrivtsovMikhail";
+const isStudent2 = true;
+const mySymbol2 = Symbol("");
+const BigNumber2 = 10000000n;
+const person2 = {Name: userName2, age: age2};
+const colors2 = ['#721422', '#98FB98', '#72525C']; console.log(colors2);
+const greet2 = function() {return "hello"; };
+const undefinedVar2 = undefined;
+const emptyValue2 = null;
+const PI2 = 3.1415;
+var oldSchool2 = "JS";
+const hasAccess = false;
+const numbers2 = [1, 2, 3, 4, 5];
+const car2 = {brand:"BMW", model:"M5", year:"2023"};
+const country2 = "Russia";
+const sayHi2 = function(){return SayHi2}; console.log(sayHi2);
+const taskCount2 = 31;
+const NAX_USERS2 = 101;
+const fruits2 = [Peach, lemon, orange];
+const temperature2 = 36.6;
+const isOnline2 = false;
+const settings2 = "";
+const messages2 = [];
+const sumt2 = 1 + 1;
+const lastLogin2 = null;
+const userRole2 = "user";
+const width = 500;
+const languages2 = ["rust", "C++", "C#"];
+const book2 = { title: "title", author: "author", year: "year"};
+
+//task 6
+
+const a = 10;
+const b = 3;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+const c = 17;
+const d = 2;
+console.log(c + d);
+console.log(c - d);
+console.log(c * d);
+console.log(c / d);
+console.log(c % d);
+
+const e = 105;
+const f = 23;
+console.log(e + f);
+console.log(e - f);
+console.log(e * f);
+console.log(e / f);
+console.log(e % f);
+
+const g = 13;
+const h = 7;
+console.log(g + h);
+console.log(g - h);
+console.log(g * h);
+console.log(g / h);
+console.log(g % h);
+
+const i = 532;
+const j = 456;
+console.log(i + j);
+console.log(i - j);
+console.log(i * j);
+console.log(i / j);
+console.log(i % j);
+
+const name1T6 = "Alice";
+const age1T6 = 25;
+
+console.log("My name is " + name1T6 + ", im " + age1T6, "years old"); 
+
+console.log(`my name is ${name1T6}, im ${age1T6} years old`);
+
+const name2T6 = "Alex"
+const age2T6 = 40;
+
+console.log("My name is " + name2T6 + ", im " + age2T6, "years old")
+console.log(`My name is ${name2T6}, im ${age2T6} years old`)
+
+const name3T6 = "Bubba";
+const age3T6 = 55;
+
+console.log("My name is " + name3T6 + ", im " + age3T6, "years old")
+console.log(`My name is ${name3T6}, im ${age3T6} years old`)
+
+const strNumber = "100";
+console.log(Number(strNumber));
+console.log(parseInt(strNumber));
+console.log(+strNumber);
+
+const strNubmer2 = "50";
+console.log(Number(strNubmer2));
+console.log(parseInt(strNubmer2));
+console.log(+strNubmer2);
+
+const strNumber3 = "200";
+console.log(Number(strNumber3));
+console.log(parseInt(strNumber3));
+console.log(+strNumber3);
+
+const strNumber4 = "0"
+console.log(Number(strNumber4));
+console.log(parseInt(strNumber4));
+console.log(+strNumber4);
+
+const strNumber5 = "-30";
+console.log(Number(strNumber5));
+console.log(parseInt(strNumber5));
+console.log(+strNumber5);
+
+const strNumber6 = "3.14";
+console.log(Number(strNumber6));
+console.log(parseFloat(strNumber6));
+console.log(+strNumber6);
+
+console.log("5" * 2);
+console.log("10" / "2");
+console.log("7" - 3);
+console.log("4" + 5)
+
+console.log("200" % 101 );
+console.log(30 + + "20");
+console.log(17 / "a");
+
+console.log(Boolean(0));
+console.log(Boolean(1));
+console.log(Boolean(""));
+console.log(Boolean("hello"));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+
+console.log(Boolean(-1));
+console.log(Boolean(3.14));
+console.log(Boolean("-1"));
+console.log(Boolean(function(){return ""}));
+console.log(Boolean(0n));
+console.log(Boolean(["","",""]))
+console.log(Boolean({Obj: -1}));
+console.log(Boolean(Symbol("")));
+console.log(Boolean(1 - 1));
+console.log(Boolean(10 * 0));
+console.log(Boolean(10 / 0));
+
+let count = 0;
+console.log(count++);
+console.log(count);
+console.log(count += 5);
+console.log(count -= 2);
+
+let count2 = 9
+let step = 1;
+while (step != 10) {
+if (count2 < 3) {
+    // console.log(count2 += 13)
+    console.log(step++)
+}
+else if (count2 > 7) {
+    // console.log(count2 -= 18)
+    console.log(step++)
+} 
+else {
+    console.log(++step);
+}
+}
+
+
+const last1 = "4";
+const last2 = 5;
+const last3 = "string";
+const lastArr = [1, 2, 3, 4];
+const lastSym = Symbol("72")
+let shift = lastArr.shift();
+
+console.log(shift + last1);
+console.log(String(lastSym));
+console.log(last1 + String(lastArr));
+console.log(String(last2) * last1);
+console.log(lastArr * lastSym);
+
+
+//day 3
+
+//21
+
+const sum21 = 21;
+const str21 = "twenty one";
+const bool21 = false;
+const null21 = null;
+const undef21 = undefined;
+const sym21 = Symbol("21");
+const big21 = 210n;
+const obj21 = {object: "21"};
+const arr21 = [21, 211, 2111];
+const func21 = function() {return 21; };
+
+//22
+
+const sum22 = 22;
+const str22 = "twenty2";
+const bool22 = true;
+const null22 = null;
+const undef22 = undefined;
+const big22 = 220n;
+const sym22 = Symbol("22");
+const obj22 = {object:"22"};
+const arr22 = [22, 222, 2222];
+const fucn22 = function() {return 22;} ;
+
+//23
+
+const sum23 = 23;
+const str23 = "twe";
+const bool23 = true;
+const null23 = null;
+const undef23 = undefined;
+const big23 = 230n;
+const sym23 = Symbol("23");
+const obj23 = {object: 23};
+const arr23 = [23, 233, 2333];
+const func23 = function(){return 23; };
+
+//24
+
+const sum24 = 24;
+const str24 = "tw4";
+const bool24 = false;
+const null24 = null;
+const undefined24 = undefined;
+const big24 = 240n;
+const sym24 = Symbol("24");
+const obj24 = {object: rfr};
+const arr24 = [24, 244, 2444];
+const func24 = function(){return zero; };
+
+//25
+
+const sum25 = 25;
+const str25 = "25w";
+const bool25 = true;
+const bull25 = null;
+const undef25 = undefined;
+const big25 = 250n;
+const sym25 = Symbol("25;f");
+const obj25 = {object: eat};
+const arr25 = [25, 255, 2555];
+const func25 = function() {return base; };
+
+//26
+
+const sum26 = 26;
+const str26 = "ster26";
+const bool26 = true;
+const null26 = null;
+const undef26 = undefined;
+const big26 = 260n;
+const sym26 = Symbol("26t");
+const obj26 = {obj: car26};
+const arr26 = [26, 266, 266];
+const func26 = function() {return zero; };
+
+//27
+
+const sum27 = 27;
+const str27 = "2seven";
+const bool27 = false;
+const null27 = null;
+const undef27 = undefined;
+const big27 = 270n;
+const sym27 = Symbol("27n");
+const obj27 = {blah: blah};
+const arr27 = [27, 277, 2777];
+const func27 = function() {return zero; };
+
+//28
+
+const sum28 = 28;
+const str28 = "28s";
+const bool28 = true;
+const null28 = null;
+const undef28 = undefined;
+const big28 = 280n;
+const sym28 = Symbol("28s");
+const obj28 = {obj: ble};
+const arr28 = [28, 288, 2888];
+const func28 = function() {return zero; };
+
+//29
+
+const sum29 = 29;
+const str29 = "29n";
+const bool29 = true;
+const null29 = null;
+const undef29 = undefined;
+const big29 = 290n;
+const sym29 = Symbol("29fv");
+const obj29 = {object: bobject};
+const arr29 = [29, 299, 2999];
+const func29 = function() {return zero; };
+
+//30
+
+const sum30 = 30;
+const str30 = "30nd";
+const bool30 = false;
+const  null30 = null;
+const undef30 = undefined;
+const big30 = 300n;
+const sym30 = Symbol("30fc");
+const obj30 = {obj: blrfef};
+const arr30 = [30, 300, 3000];
+const func30 = function() {return big1; };
+
+
+
+const age3 = 20;
+const userName3 = "MishaKrivtsov";
+const isStudent3 = true;
+const mySymbol3 = Symbol("");
+const bigNumber3 = 10000000n;
+const person3 = {Name: userName3, age: age3};
+const colors3 = ['#EFCDB8', '#D68A59', '#F0FFFF']; 
+const greet3 = function() {return "Hello"};
+const undefinedVar3 = undefined;
+const emptyValue3 = null;
+const PI3 = 3.1415;
+var oldShool3 = "JS";
+const hasAccess3 = false;
+const car3 = {model: "Sportage", brand: "KIA", year: 2018 };
+const country3 = "Russia";
+const sayHi3 = function() {return bublik; }; console.log(sayHi3);
+const taskCount3 = 30;
+const MAX_USERS3 = 103;
+const fruits3 =  ["pomegrade", "pear", "apple"];
+const temperature3 = 33.3;
+const isOnline3 = true;
+const settings3 = "";
+const messages3 = [];
+const sum3T6 = 345 + 45345;
+const lastLogin3 = null;
+const userRole3 = "manager";
+const lenght = 300;
+const lamguages3 = ["pascal", "basic", "C"];
+const book3 = {title: "title", author: "author", year: "year"};
+
+// test
